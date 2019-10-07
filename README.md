b16d5ac2e670f0b4732d6b3f8be3145c170aac97
lmontier <laurent.montier@gmail.com>
2019-10-07 11:18:50 +0200

    Add val and test, and score

--- README.md
+++
@@ -1,3 +0,0 @@
-# transfer-learning
-This repo contains the code to support the blog on transfer learning. [Here](https://towardsdatascience.com/keras-transfer-learning-for-beginners-6c9b8b7143e) 
-Transfer learning using the MobileNet pretrained network available in keras.applications which has been trained on the imagenet dataset.
--- transfer-learning.py
+++ transfer-learning.py
@@ -1,77 +1,71 @@
-
 # coding: utf-8
 
 # In[1]:
-
-
 import pandas as pd
-import numpy as np
-import os
-import keras
-import matplotlib.pyplot as plt
-from keras.layers import Dense,GlobalAveragePooling2D
 from keras.applications import MobileNet
-from keras.preprocessing import image
 from keras.applications.mobilenet import preprocess_input
-from keras.preprocessing.image import ImageDataGenerator
+from keras.layers import Dense, GlobalAveragePooling2D
 from keras.models import Model
-from keras.optimizers import Adam
-
+from keras.preprocessing.image import ImageDataGenerator
 
 # In[2]:
 
 
-base_model=MobileNet(weights='imagenet',include_top=False) #imports the mobilenet model and discards the last 1000 neuron layer.
+base_model = MobileNet(weights='imagenet',
+                       include_top=False)  #imports the mobilenet model and discards the last 1000 neuron layer.
 
-x=base_model.output
-x=GlobalAveragePooling2D()(x)
-x=Dense(1024,activation='relu')(x) #we add dense layers so that the model can learn more complex functions and classify for better results.
-x=Dense(1024,activation='relu')(x) #dense layer 2
-x=Dense(512,activation='relu')(x) #dense layer 3
-preds=Dense(3,activation='softmax')(x) #final layer with softmax activation
+x = base_model.output
+x = GlobalAveragePooling2D()(x)
+x = Dense(1024, activation='relu')(
+    x)  #we add dense layers so that the model can learn more complex functions and classify for better results.
+x = Dense(1024, activation='relu')(x)  #dense layer 2
+x = Dense(512, activation='relu')(x)  #dense layer 3
+preds = Dense(3, activation='softmax')(x)  #final layer with softmax activation
 
-
-# In[3]:
-
-
-model=Model(inputs=base_model.input,outputs=preds)
-#specify the inputs
-#specify the outputs
-#now a model has been created based on our architecture
-
-
-# In[4]:
-
+model = Model(inputs=base_model.input, outputs=preds)
 
 for layer in model.layers[:20]:
-    layer.trainable=False
+    layer.trainable = False
 for layer in model.layers[20:]:
-    layer.trainable=True
+    layer.trainable = True
 
+datagen = ImageDataGenerator(preprocessing_function=preprocess_input)
 
-# In[5]:
+train_generator = datagen.flow_from_directory(
+    './data/train/',
+    target_size=(224, 224),
+    color_mode='rgb',
+    batch_size=8,
+    class_mode='categorical',
+    shuffle=True
+)
+val_generator = datagen.flow_from_directory(
+    './data/val/',
+    target_size=(224, 224),
+    color_mode='rgb',
+    batch_size=8,
+    class_mode='categorical',
+    shuffle=True
+)
+test_generator = datagen.flow_from_directory(
+    './data/test/',
+    target_size=(224, 224),
+    color_mode='rgb',
+    batch_size=8,
+    class_mode='categorical',
+    shuffle=True
+)
 
+model.compile(optimizer='Adam', loss='categorical_crossentropy', metrics=['accuracy'])
+step_size_train = train_generator.n // train_generator.batch_size
 
-train_datagen=ImageDataGenerator(preprocessing_function=preprocess_input) #included in our dependencies
+model.fit_generator(
+    generator=train_generator,
+    validation_data=val_generator,
+    steps_per_epoch=step_size_train,
+    epochs=5
+)
 
-train_generator=train_datagen.flow_from_directory('./train/', # this is where you specify the path to the main data folder
-                                                 target_size=(224,224),
-                                                 color_mode='rgb',
-                                                 batch_size=32,
-                                                 class_mode='categorical',
-                                                 shuffle=True)
-
-
-# In[33]:
-
-
-model.compile(optimizer='Adam',loss='categorical_crossentropy',metrics=['accuracy'])
-# Adam optimizer
-# loss function will be categorical cross entropy
-# evaluation metric will be accuracy
-
-step_size_train=train_generator.n//train_generator.batch_size
-model.fit_generator(generator=train_generator,
-                   steps_per_epoch=step_size_train,
-                   epochs=5)
-
+score = model.evaluate_generator(test_generator)
+df = pd.DataFrame().assign(test_loss=[score[0]], test_accuracy=[score[1]]).to_csv('./output/test_results.csv')
+model.save_weights('./output/my_model_weights.h5')
